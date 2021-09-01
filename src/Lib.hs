9d679f3b636ef8134bfe4c86db28631a14e2237d
Enzo Cioppettini <enzoc4@gmail.com>
2021-09-01 01:11:14 -0300

    not working

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,155 +1,128 @@
-{-@ LIQUID "--no-termination" @-}
 {-@ LIQUID "--prune-unsorted" @-}
 {-@ LIQUID "--ple" @-}
 {-@ LIQUID "--reflection" @-}
+{-@ LIQUID "--no-adt" @-}
 
-
-{-# LANGUAGE NoMonomorphismRestriction, PartialTypeSignatures #-}
-{-# LANGUAGE ConstraintKinds, FlexibleContexts,InstanceSigs,
-             TypeFamilies, UndecidableInstances, EmptyDataDecls, ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilyDependencies  #-}
+{-# LANGUAGE UndecidableSuperClasses #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Lib where
-import Data.Typeable
-import Control.Concurrent.Chan
-import Control.Concurrent
-import Prelude hiding (break)
-import PreSessions
+import Control.Concurrent (Chan, MVar, forkIO, newEmptyMVar, putMVar, takeMVar)
+import Prelude
+import Data.Bifunctor (Bifunctor(bimap))
+import Data.Functor ((<$>))
+import Data.Tuple (swap)
+import Control.Monad (void)
 
 mainFunc = do
+    example0
     putStrLn "hello world"
-
 
 client0 s = do
     s <- send' 5 s
-    (y, s) <- recv s
-    close s
+    (y, s) <- recv' s
+    close' s
+    putStrLn "end client"
+
+{-@ type Odd = {i: Int | i mod 2 = 0 }@-}
+{-@ type Even = {i: Int | i mod 2 = 1 }@-}
 
 {-@ server0 :: (Recv Odd (Send Even End)) -> IO () @-}
 server0 :: (Recv Int (Send Int End)) -> IO ()
 server0 s = do
-    (x, s) <- recv s
+    (x, s) <- recv' s
     s <- send' (x + 1) s 
-    close s
-    putStrLn "End"
+    close' s
+    -- putStrLn "end server"
 
--- example = connect client0 server0 
+example0 = connect' client0 server0
 
--- data Send a t = Send a (Chan t)
--- {-@ data Send a t = Send a (Chan t)   @-}
--- 
--- 
--- data Recv a t = Recv a (Chan t)
--- data End
--- 
--- {-@ send :: x:a -> Chan (Send {v : a  | v = x } t) -> IO (Chan t)  @-}
--- send ::  a -> Chan (Send a t) -> IO (Chan t)
--- send  x c = do
---     c' <- newChan
---     writeChan  c (Send x  c')
---     return c'
---     
--- {-@ recv ::Chan (Recv a t) -> IO (a, Chan t)  @-}
--- recv :: Chan (Recv a t) -> IO (a, Chan t)
--- recv c = do
---     (Recv a c') <- readChan c
---     return (a, c')
--- 
--- close :: Chan End -> IO ()
--- close c = return ()
--- 
--- {-@ fork :: Link s => (Chan s -> IO ()) -> IO (Chan (Dual s))@-}
--- fork :: Link s => (Chan s -> IO ()) -> IO (Chan (Dual s))
--- fork f = do
---     c <- newChan
---     c' <- newChan
---     forkIO $ link c c'
---     forkIO (f c)
---     return c'
--- 
--- newS :: Link s => IO (Chan s , Chan (Dual s))
--- newS  = do
---     c <- newChan
---     c' <- newChan
---     forkIO $ link c c'
---     return (c,c')
--- 
--- 
--- type family
---     Dual s where
---     Dual (Send a t) = Recv a (Dual t)
---     Dual (Recv a t) = Send a (Dual t)
---     Dual End        = End
--- 
--- class Link s where
---     link :: Chan s -> Chan (Dual s) -> IO ()
--- 
--- instance Link t => Link (Send a t) where
---     link c0  c1 = do
---         (Send x c0') <- readChan c0
---         c1' <- newChan
---         forkIO $ link c0' c1'
---         writeChan c1 (Recv x c1')
--- 
--- instance (Link t) => Link (Recv a t) where
---     link c0 c1 = do
---         (Send x c1') <- readChan c1
---         c0' <- newChan
---         forkIO $ link c0' c1'
---         writeChan c0 (Recv x c0')
--- 
--- instance Link End where
---     link c0 c1 = return ()
--- 
--- 
--- -- Basic Example
--- 
--- {-@ data variance Recv covariant covariant @-}
--- {-@ data variance Send contravariant covariant @-}
--- {-@ data variance Chan covariant @-}
--- 
--- 
--- -- {-@ client0 :: Chan (Send Odd (Recv Even End)) -> IO() @-}
--- -- client0 :: Chan (Send Int (Recv Int End)) -> IO()
--- 
--- client0 s = do
---     s <- send 5 s
---     (y, s) <- recv s
---     close s
--- 
--- {-@ server0 :: Chan (Recv Odd (Send Even End)) -> IO() @-}
--- server0 :: Chan(Recv Int (Send Int End)) -> IO()
--- server0 s = do
---     (x, s) <- recv s
---     s <- send (x+1) s -- x+2 es unsafe 
---     close s
---     putStrLn "End"
--- 
--- {-@ ignore example0 @-}
--- jxample0 = do
---     c' <- fork server0
---     return c'
---     client0 c'
---     return ()
--- 
--- ---------------------------------
--- -- Some dependencies
--- ---------------------------------
--- 
--- 
--- data C b = C { cont ::  b}
--- {-@ data C b = C { cont :: b} @-}
--- 
--- 
--- data Act a b  = Act { p :: a, c :: C b }
--- 
--- {-@ data Act a b <f :: a -> Bool, g ::a -> b -> Bool >  =
---        Act {p :: a<f p>
---            ,c :: C (b <g p>) }
--- @-}
--- 
--- 
--- {-@ myC :: Act <{\x -> x > 1},{\x y ->  x < (p y) && (p y) < (p (cont (c y)))}> Int (Act Int (Act Int Int)) @-}
--- -- {-@ myC :: Act <{\x y ->  x < (p y) && (p y) < (p (cont (c y)))}> Int (Act Int (Act Int Int)) @-}
--- myC :: Act Int (Act Int (Act Int Int))
--- myC = Act 2 (C (Act 3 (C (Act 7 (C 0)))))
--- 
+-- * Session types
+
+data Send a s = Send (SendOnce (a, Dual s))
+data Recv a s = Recv (RecvOnce (a, s))
+data End      = End SyncOnce
+
+{-@ data variance Recv covariant covariant @-}
+{-@ data variance Send contravariant covariant @-}
+
+-- * Duality and session initiation
+
+-- con esto ghc da stack overflow, o sea, Session (Dual s)
+-- class ( Session (Dual s), Dual (Dual s) ~ s) => Session s where
+class (Dual (Dual s) ~ s) => Session s where
+  type Dual s = result | result -> s
+  newS :: IO (s, Dual s)
+
+instance Session s => Session (Send a s) where
+  type Dual (Send a s) = Recv a (Dual s)
+  newS = bimap Send Recv <$> onew
+
+instance Session s => Session (Recv a s) where
+  type Dual (Recv a s) = Send a (Dual s)
+  newS = bimap Recv Send . swap <$> onew
+
+instance Session End where
+  type Dual End = End
+  newS = bimap End End <$> onewSync
+
+instance Session () where
+  type Dual () = ()
+  newS = return ((), ())
+
+
+-- * Communication primitives
+
+{-@ send' :: Session s => v1:a -> Send {v2:a | v1 = v2 } s -> IO s @-}
+send' :: Session s => a -> Send a s -> IO s
+send' x (Send ch_s) = do
+  (here, there) <- newS
+  osend ch_s (x, there)
+  return here
+
+recv' :: Recv a s -> IO (a, s)
+recv' (Recv ch_r) = orecv ch_r
+
+close' :: End -> IO ()
+close' (End sync) = osync sync
+
+connect' :: (Session s) => (s -> IO ()) -> (Dual s -> IO a) -> IO a
+connect' k1 k2 = do (s1, s2) <- newS; void (forkIO (k1 s1)); k2 s2
+
+-- * One-shot channels
+
+data SendOnce a = SendOnce (MVar a)
+data RecvOnce a = RecvOnce (MVar a)
+
+{-@ data variance SendOnce covariant @-}
+{-@ data variance RecvOnce covariant @-}
+
+onew :: IO (SendOnce a, RecvOnce a)
+onew = bimap SendOnce RecvOnce <$> dup2 newEmptyMVar
+    where 
+        -- In the linear version, this comes from somewhere else
+        dup2 :: IO (MVar a) -> IO (MVar a, MVar a)
+        dup2 first = do
+            f <- first 
+            return (f, f)
+
+osend :: SendOnce a -> a -> IO ()
+osend (SendOnce mvar) = putMVar mvar
+
+orecv :: RecvOnce a -> IO a
+orecv (RecvOnce mvar) = takeMVar mvar
+
+-- * Synchronisation construct
+
+data SyncOnce = SyncOnce (SendOnce ()) (RecvOnce ())
+
+onewSync :: IO (SyncOnce, SyncOnce)
+onewSync = do
+  (ch_s1, ch_r1) <- onew
+  (ch_s2, ch_r2) <- onew
+  return (SyncOnce ch_s1 ch_r2, SyncOnce ch_s2 ch_r1)
+
+osync :: SyncOnce -> IO ()
+osync (SyncOnce ch_s ch_r) = do osend ch_s (); orecv ch_r
