e2e1ff38e7ac0109dab8291d5f1a2cc96a206ca9
Enzo Cioppettini <enzoc4@gmail.com>
2021-09-09 01:52:14 -0300

    now compiling

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Lib
+import Control.Concurrent.MVar
 
 main :: IO ()
 main = mainFunc
--- src/Lib.hs
+++ src/Lib.hs
@@ -18,13 +18,11 @@ import Control.Monad (void)
 
 mainFunc = do
     example0
-    putStrLn "hello world"
 
 client0 s = do
-    s <- send' 5 s
-    (y, s) <- recv' s
-    close' s
-    putStrLn "end client"
+    s <- send 5 s
+    (y, s) <- recv s
+    close s
 
 {-@ type Odd = {i: Int | i mod 2 = 0 }@-}
 {-@ type Even = {i: Int | i mod 2 = 1 }@-}
@@ -32,12 +30,11 @@ client0 s = do
 {-@ server0 :: (Recv Odd (Send Even End)) -> IO () @-}
 server0 :: (Recv Int (Send Int End)) -> IO ()
 server0 s = do
-    (x, s) <- recv' s
-    s <- send' (x + 1) s 
-    close' s
-    -- putStrLn "end server"
+    (x, s) <- recv s
+    s <- send (x + 1) s 
+    close s
 
-example0 = connect' client0 server0
+example0 = connect client0 server0
 
 -- * Session types
 
@@ -58,15 +55,15 @@ class (Dual (Dual s) ~ s) => Session s where
 
 instance Session s => Session (Send a s) where
   type Dual (Send a s) = Recv a (Dual s)
-  newS = bimap Send Recv <$> onew
+  newS = bimap Send Recv <$> new'
 
 instance Session s => Session (Recv a s) where
   type Dual (Recv a s) = Send a (Dual s)
-  newS = bimap Recv Send . swap <$> onew
+  newS = bimap Recv Send . swap <$> new'
 
 instance Session End where
   type Dual End = End
-  newS = bimap End End <$> onewSync
+  newS = bimap End End <$> newSync
 
 instance Session () where
   type Dual () = ()
@@ -75,21 +72,21 @@ instance Session () where
 
 -- * Communication primitives
 
-{-@ send' :: Session s => v1:a -> Send {v2:a | v1 = v2 } s -> IO s @-}
-send' :: Session s => a -> Send a s -> IO s
-send' x (Send ch_s) = do
+{-@ send :: Session s => v1:a -> Send {v2:a | v1 = v2 } s -> IO s @-}
+send :: Session s => a -> Send a s -> IO s
+send x (Send ch_s) = do
   (here, there) <- newS
-  osend ch_s (x, there)
+  send' ch_s (x, there)
   return here
 
-recv' :: Recv a s -> IO (a, s)
-recv' (Recv ch_r) = orecv ch_r
+recv :: Recv a s -> IO (a, s)
+recv (Recv ch_r) = recv' ch_r
 
-close' :: End -> IO ()
-close' (End sync) = osync sync
+close :: End -> IO ()
+close (End s) = sync s
 
-connect' :: (Session s) => (s -> IO ()) -> (Dual s -> IO a) -> IO a
-connect' k1 k2 = do (s1, s2) <- newS; void (forkIO (k1 s1)); k2 s2
+connect :: (Session s) => (s -> IO ()) -> (Dual s -> IO a) -> IO a
+connect k1 k2 = do (s1, s2) <- newS; void (forkIO (k1 s1)); k2 s2
 
 -- * One-shot channels
 
@@ -99,8 +96,8 @@ data RecvOnce a = RecvOnce (MVar a)
 {-@ data variance SendOnce covariant @-}
 {-@ data variance RecvOnce covariant @-}
 
-onew :: IO (SendOnce a, RecvOnce a)
-onew = bimap SendOnce RecvOnce <$> dup2 newEmptyMVar
+new' :: IO (SendOnce a, RecvOnce a)
+new' = bimap SendOnce RecvOnce <$> dup2 newEmptyMVar
     where 
         -- In the linear version, this comes from somewhere else
         dup2 :: IO (MVar a) -> IO (MVar a, MVar a)
@@ -108,21 +105,23 @@ onew = bimap SendOnce RecvOnce <$> dup2 newEmptyMVar
             f <- first 
             return (f, f)
 
-osend :: SendOnce a -> a -> IO ()
-osend (SendOnce mvar) = putMVar mvar
+send' :: SendOnce a -> a -> IO ()
+send' (SendOnce mvar) = putMVar mvar
 
-orecv :: RecvOnce a -> IO a
-orecv (RecvOnce mvar) = takeMVar mvar
+recv' :: RecvOnce a -> IO a
+recv' (RecvOnce mvar) = takeMVar mvar
 
 -- * Synchronisation construct
 
 data SyncOnce = SyncOnce (SendOnce ()) (RecvOnce ())
 
-onewSync :: IO (SyncOnce, SyncOnce)
-onewSync = do
-  (ch_s1, ch_r1) <- onew
-  (ch_s2, ch_r2) <- onew
+newSync :: IO (SyncOnce, SyncOnce)
+newSync = do
+  (ch_s1, ch_r1) <- new'
+  (ch_s2, ch_r2) <- new'
   return (SyncOnce ch_s1 ch_r2, SyncOnce ch_s2 ch_r1)
 
-osync :: SyncOnce -> IO ()
-osync (SyncOnce ch_s ch_r) = do osend ch_s (); orecv ch_r
+sync :: SyncOnce -> IO ()
+sync (SyncOnce ch_s ch_r) = do send' ch_s (); recv' ch_r
+
+-- * Contracts
