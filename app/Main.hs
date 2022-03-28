67fd338ebc1b43ebf29365a52cade4937d97eb02
Enzo Cioppettini <enzoc4@gmail.com>
2022-03-28 01:02:46 -0300

    unsound

--- app/Main.hs
+++ app/Main.hs
@@ -1,21 +1,16 @@
-{-@ LIQUID "--prune-unsorted" @-}
-{-@ LIQUID "--ple" @-}
-{-@ LIQUID "--reflection" @-}
-{-@ LIQUID "--no-pattern-inline" @-}
-{-@ LIQUID "--no-adt" @-}
-{-@ LIQUID "--exact-data-cons" @-}
-
-{-@ LIQUID "--short-names" @-}
-{-@ LIQUID "--no-pattern-inline" @-}
+--{-@ LIQUID "--prune-unsorted" @-}
+--{-@ LIQUID "--ple" @-}
+--{-@ LIQUID "--reflection" @-}
+--{-@ LIQUID "--no-pattern-inline" @-}
+--{-@ LIQUID "--no-adt" @-}
+--{-@ LIQUID "--exact-data-cons" @-}
+--
+--{-@ LIQUID "--short-names" @-}
+--{-@ LIQUID "--no-pattern-inline" @-}
 
 module Main where
 
 import Lib
-import Control.Concurrent.MVar
-import RIO
-import qualified Data.Set as Set
-import Data.Map
-import Data.Map(Map)
 
 main :: IO ()
 main = mainFunc
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,148 +1,157 @@
-{-@ LIQUID "--prune-unsorted" @-}
-{-@ LIQUID "--ple" @-}
-{-@ LIQUID "--reflection" @-}
 {-@ LIQUID "--no-pattern-inline" @-}
-{-@ LIQUID "--no-adt" @-}
-{-@ LIQUID "--exact-data-cons" @-}
-{-@ LIQUID "--higherorder" @-}
-{-@ LIQUID "--no-termination" @-}
--- {-@ LIQUID "--diffcheck" @-}
-
-{-# LANGUAGE TypeFamilyDependencies  #-}
-{-# LANGUAGE UndecidableSuperClasses #-}
-{-# LANGUAGE NoMonomorphismRestriction #-}
-{-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE InstanceSigs #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE FunctionalDependencies #-}
 
 module Lib where
-import Control.Concurrent (Chan, MVar, forkIO, newEmptyMVar, putMVar, takeMVar)
+
 import Prelude
-import Data.Bifunctor (Bifunctor(bimap))
-import Data.Functor ((<$>))
-import Control.Applicative ((<|>))
-import Data.Tuple (swap)
-import Control.Monad (void)
-import RIO (RIO (RIO), rState, World (W), liftRIO)
-import qualified Data.Set as Set
-import qualified Data.Set as Map
-import Data.Map (Map)
-import Unsafe.Coerce (unsafeCoerce)
-
-mainFunc = do
-    putStrLn "running session examples"
-    sumSesh
-
--- variables de estado
+import Control.Applicative
 
 {-@ measure lastVal :: World -> Int @-}
 
-{-@ predicate EmptyWorld W = lastVal W = 0 @-}
-
-{-@ predicate NextWorld W1 V W2 = lastVal W2 = V @-}
-
--- helper
 {-@ predicate Pure W1 W2 = W1 = W2 @-}
 
-
-{-@ assume emptyWorld :: {w:World | EmptyWorld w} @-}
+{-@ assume emptyWorld :: {w:World | lastVal w = 0} @-}
 emptyWorld :: World
 emptyWorld = W
 
-{-@ assume doA :: Session s => v1:Int -> Send Int s -> RIO<{\w -> lastVal w < v1}, {\w1 b w2 -> lastVal w2 = v1}> s @-}
-doA :: Session s => Int -> Send Int s -> RIO s
-doA i (Send s) = do
+{-@ assume doA :: Typestate s => v1:Int -> A Int s -> RIO<{\w -> lastVal w < v1}, {\w1 b w2 -> lastVal w2 = v1}> s @-}
+doA :: Typestate s => Int -> A Int s -> RIO s
+doA i (A s) = do
     liftRIO $ print i
     return s
 
-newtype SumCnt
-  = MkSumCnt (Select (Send Int SumCnt) (Recv Int End))
+{-@ assume doB :: B s -> RIO<{\w -> true}, {\w1 v w2 -> true}> s @-}
+doB :: B s -> RIO s
+doB (B s) = do 
+    liftRIO $ putStrLn "doing B"
+    return s
 
-{-@ sumCnt :: SumCnt -> RIO <{\w -> EmptyWorld w}> () @-}
-sumCnt :: SumCnt -> RIO ()
-sumCnt (MkSumCnt s) = do 
+{-@ end :: End -> RIO <{\x -> true}, {\w1 b w2 -> Pure w1 w2}> () @-}
+end :: End -> RIO ()
+end (End s) = return ()
+
+{-@ assume selectLeft :: Typestate s => Branch s_1 s_2 -> RIO<{\w1 -> true}, {\w1 v w2 -> Pure w1 w2 }> s_1 @-}
+selectLeft :: (Typestate s_1) => Branch s_1 s_2 -> RIO s_1
+selectLeft (Branch s _) = return s
+
+{-@ assume selectRight :: Typestate s => Branch s_1 s_2 -> RIO<{\w1 -> true}, {\w1 v w2 -> Pure w1 w2}> s_2 @-}
+selectRight :: (Typestate s_2) => Branch s_1 s_2 -> RIO s_2
+selectRight (Branch _ s) = return s
+
+newtype Rec
+  = MkRec (Branch (A Int Rec) (B End))
+
+{-@ f :: Rec -> RIO <{\w -> lastVal w = 0}> () @-}
+f :: Rec -> RIO ()
+f (MkRec s) = do 
     s <- selectLeft s
-    MkSumCnt s <- doA 100 s
+    MkRec s <- doA 100 s
     s <- selectLeft s
-    MkSumCnt s <- doA 200 s
+    MkRec s <- doA 100 s
     s <- selectLeft s
-    MkSumCnt s <- doA 300 s
-    s <- selectLeft s
-    MkSumCnt s <- doA 400 s
+    MkRec s <- doA 300 s
     --s <- selectLeft s
-    --MkSumCnt s <- doA 500 s
+    --MkRec s <- doA 400 s
+    --s <- selectLeft s
+    --MkRec s <- doA 500 s
     s <- selectRight s
-    (tot, s) <- recv s
+    s <- doB s
 
-    liftRIO $ print tot
-    close s
+    end s
 
-
-instance Session SumCnt
-  where
-    newS = MkSumCnt $ newS 
-
-sumSesh = fmap fst $ rState (sumCnt newS) $ emptyWorld 
+mainFunc = fmap fst $ rState (f newS) $ emptyWorld 
 
 
--- * Session types
---
-{-@ data Send a s = Send s @-}
-data Send a s = Send s
+{-@ data A a s = A s @-}
+data A a s = A s
 
-{-@ data Recv a s = Recv s @-}
-data Recv a s = Recv s
+{-@ data B s = B s @-}
+data B s = B s
 data End      = End ()
 
+{-@ data Branch c1 c2 = Branch c1 c2 @-}
+data Branch c1 c2 = Branch c1 c2
+
+
+-- recursive constructors
 --
-{-@ data variance Recv covariant covariant @-}
-{-@ data variance Send contravariant covariant @-}
-
-
--- * Duality and session initiation
-
-class Session s where
+class Typestate s where
   newS :: s
 
-instance Session s => Session (Send a s) where
-  newS = Send newS
+instance Typestate s => Typestate (A a s) where
+  newS = A newS
 
-instance Session s => Session (Recv a s) where
-  newS = Recv newS
+instance Typestate s => Typestate (B s) where
+  newS = B newS
 
-instance Session End where
+instance Typestate End where
   newS = End ()
 
-instance Session () where
+instance Typestate () where
   newS = ()
 
+instance (Typestate s1, Typestate s2) => Typestate (Branch s1 s2) where
+  newS = Branch newS newS 
 
--- * Communication primitives
+instance Typestate Rec
+  where
+    newS = MkRec $ newS 
 
-{-@ assume recv :: Recv Int s -> RIO<{\w -> true}, {\w1 v w2 -> NextWorld w1 (fst v) w2}> (Int, s) @-}
-recv :: Recv Int s -> RIO (Int, s)
-recv (Recv s) = return (1, s)
 
-{-@ close :: End -> RIO <{\x -> true}, {\w1 b w2 -> Pure w1 w2}> () @-}
-close :: End -> RIO ()
-close (End s) = return ()
+{-@ liftRIO :: IO a -> RIO<{\w -> true}, {\w1 v w2 -> w1 == w2 }> a @-}
+liftRIO :: IO a -> RIO a
+liftRIO m = RIO $ \w -> (fmap (\v -> (v, w)) m)
 
---
--- branching
+-- RIO
 
-type Select s_1 s_2 = Send (Either (s_1) (s_2)) ()
+{-@ data RIO a <p :: World -> Bool, q :: World -> a -> World -> Bool>
+      = RIO (runState :: (x:World<p> -> IO (a, World)<\w -> {v:World<q x w> | true}>))
+  @-}
+data RIO a  = RIO {runState :: World -> IO (a, World)}
 
-{-@ assume selectLeft :: Session s => Select s_1 s_2 -> RIO<{\w1 -> true}, {\w1 v w2 -> Pure w1 w2 }> s_1 @-}
-selectLeft :: (Session s_1) => Select s_1 s_2 -> RIO s_1
-selectLeft (Send s) = s 
+{-@ rState :: forall <p :: World -> Bool, q :: World -> a -> World -> Bool>.
+                RIO <p, q> a -> x:World<p> -> IO (a, World)<\w -> {v:World<q x w> | true}> @-}
+rState (RIO f)= f
 
-{-@ assume selectRight :: Session s => Select s_1 s_2 -> RIO<{\w1 -> true}, {\w1 v w2 -> Pure w1 w2}> s_2 @-}
-selectRight :: (Session s_2) => Select s_1 s_2 -> RIO s_2
-selectRight (Send s) = s 
+data World = W
+{-@ data World = W @-}
 
+-- | RJ: Putting these in to get GHC 7.10 to not fuss
+instance Functor RIO where
+  fmap = undefined
+
+-- | RJ: Putting these in to get GHC 7.10 to not fuss
+instance Applicative RIO where
+  pure  = undefined
+  (<*>) = undefined
+
+instance Monad RIO where
+{-@ instance Monad RIO where
+      >>= :: forall < p  :: World -> Bool 
+                    , p2 :: a -> World -> Bool
+                    , r  :: a -> Bool
+                    , q1 :: World -> a -> World -> Bool
+                    , q2 :: a -> World -> b -> World -> Bool
+                    , q  :: World -> b -> World -> Bool>.
+           {x::a<r>, w::World<p>|- World<q1 w x> <: World<p2 x>}
+           {w::World<p>, x::a<r>, w1:: World<q1 w x>, w2::{v:World<p2 x> | v == w1}, y::b|- World<q2 x w2 y> <: World<q w y>}
+           {x::a, w::World<p>, w2::World<q1 w x>|- {v:a | v = x} <: a<r>}
+           RIO <p, q1> a
+        -> (x:a<r> -> RIO <{v:World<p2 x> | true}, \w1 y -> {v:World<q2 x w1 y> | true}> b)
+        -> RIO <p, q> b ;
+      >>  :: forall < p   :: World -> Bool
+                    , p2  :: World -> Bool
+                    , q1 :: World -> a -> World -> Bool
+                    , q2 :: World -> b -> World -> Bool
+                    , q :: World -> b -> World -> Bool>.
+            {x::a, w::World<p>|- World<q1 w x> <: World<p2>}
+            {w::World<p>, w2::World<p2>, y::a, w3:: {v:World<q1 w y> | v == w2}, x::b |- World<q2 w2 x> <: World<q w x>}
+            RIO <p, q1> a
+         -> RIO <p2, q2> b
+         -> RIO <p, q> b  ;
+      return :: forall <p :: World -> Bool>.
+                x:a -> RIO <p, \w0 y -> {w1:World | w0 == w1 && y == x}> a
+  @-}
+  (RIO g) >>= f = RIO $ \x -> do
+    (v, w) <- g x
+    runState (f v) w
+  return w      = RIO $ \x -> return (w, x)
