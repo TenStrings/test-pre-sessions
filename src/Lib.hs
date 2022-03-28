83b07a91dff61a19ee1d677e25e85aad6697b693
Enzo Cioppettini <enzoc4@gmail.com>
2022-03-27 21:48:05 -0300

    checkpoint2

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,7 +6,7 @@
 {-@ LIQUID "--exact-data-cons" @-}
 {-@ LIQUID "--higherorder" @-}
 {-@ LIQUID "--no-termination" @-}
-{-@ LIQUID "--diffcheck" @-}
+-- {-@ LIQUID "--diffcheck" @-}
 
 {-# LANGUAGE TypeFamilyDependencies  #-}
 {-# LANGUAGE UndecidableSuperClasses #-}
@@ -45,40 +45,38 @@ mainFunc = do
 
 {-@ predicate EmptyWorld W = lastVal W = 0 @-}
 
-{-@ predicate NextWorld W1 V W2 = LastValT W1 V W2 @-}
+{-@ predicate NextWorld W1 V W2 = lastVal W2 = V @-}
 
 -- helper
 {-@ predicate Pure W1 W2 = W1 = W2 @-}
 
 
-data Asc
-data Sum
-data Any
+{-@ assume emptyWorld :: {w:World | EmptyWorld w} @-}
+emptyWorld :: World
+emptyWorld = W
 
-{-@ assume doA :: Session s => v1:Int -> Send s -> RIO<{\w -> lastVal w < v1}, {\w1 b w2 -> lastVal w2 = v1}> s @-}
-doA :: Session s => Int -> Send s -> RIO s
-doA = undefined
+{-@ assume doA :: Session s => v1:Int -> Send Int s -> RIO<{\w -> lastVal w < v1}, {\w1 b w2 -> lastVal w2 = v1}> s @-}
+doA :: Session s => Int -> Send Int s -> RIO s
+doA i (Send s) = do
+    liftRIO $ print i
+    return s
 
--- ex 2
---
 newtype SumCnt
-  = MkSumCnt (Select (Send SumCnt) (Recv End))
+  = MkSumCnt (Select (Send Int SumCnt) (Recv Int End))
 
 {-@ sumCnt :: SumCnt -> RIO <{\w -> EmptyWorld w}> () @-}
 sumCnt :: SumCnt -> RIO ()
 sumCnt (MkSumCnt s) = do 
     s <- selectLeft s
-    MkSumCnt s <- sendAsc 100 s
+    MkSumCnt s <- doA 100 s
     s <- selectLeft s
-    MkSumCnt s <- sendAsc 200 s
+    MkSumCnt s <- doA 200 s
     s <- selectLeft s
-    MkSumCnt s <- sendAsc 300 s
+    MkSumCnt s <- doA 300 s
     s <- selectLeft s
-    MkSumCnt s <- sendAsc 400 s
-    s <- selectLeft s
-    MkSumCnt s <- sendAsc 500 s
-    s <- selectLeft s
-    MkSumCnt s <- sendAsc 600 s
+    MkSumCnt s <- doA 400 s
+    --s <- selectLeft s
+    --MkSumCnt s <- doA 500 s
     s <- selectRight s
     (tot, s) <- recv s
 
@@ -88,22 +86,18 @@ sumCnt (MkSumCnt s) = do
 
 instance Session SumCnt
   where
-    newS = undefined 
+    newS = MkSumCnt $ newS 
 
-s = undefined
-
-sumSesh = fmap fst $ rState (newS >>= sumCnt) $ W
+sumSesh = fmap fst $ rState (sumCnt newS) $ emptyWorld 
 
 
 -- * Session types
 --
-data C a
+{-@ data Send a s = Send s @-}
+data Send a s = Send s
 
-{-@ data Send s = Send s @-}
-data Send s = Send s
-
-{-@ data Recv a s = Recv (C (a, s)) @-}
-data Recv a s = Recv (C (a, s))
+{-@ data Recv a s = Recv s @-}
+data Recv a s = Recv s
 data End      = End ()
 
 --
@@ -114,30 +108,30 @@ data End      = End ()
 -- * Duality and session initiation
 
 class Session s where
-  newS :: RIO s
+  newS :: s
 
-instance Session s => Session (Send s) where
-  newS = undefined
+instance Session s => Session (Send a s) where
+  newS = Send newS
 
-instance Session s => Session (Recv s) where
-  newS = undefined
+instance Session s => Session (Recv a s) where
+  newS = Recv newS
 
 instance Session End where
-  newS = undefined
+  newS = End ()
 
 instance Session () where
-  newS = undefined
+  newS = ()
 
 
 -- * Communication primitives
 
 {-@ assume recv :: Recv Int s -> RIO<{\w -> true}, {\w1 v w2 -> NextWorld w1 (fst v) w2}> (Int, s) @-}
 recv :: Recv Int s -> RIO (Int, s)
-recv = undefined
+recv (Recv s) = return (1, s)
 
 {-@ close :: End -> RIO <{\x -> true}, {\w1 b w2 -> Pure w1 w2}> () @-}
 close :: End -> RIO ()
-close (End s) = undefined
+close (End s) = return ()
 
 --
 -- branching
@@ -146,11 +140,9 @@ type Select s_1 s_2 = Send (Either (s_1) (s_2)) ()
 
 {-@ assume selectLeft :: Session s => Select s_1 s_2 -> RIO<{\w1 -> true}, {\w1 v w2 -> Pure w1 w2 }> s_1 @-}
 selectLeft :: (Session s_1) => Select s_1 s_2 -> RIO s_1
-selectLeft (Send s) = undefined 
+selectLeft (Send s) = s 
 
 {-@ assume selectRight :: Session s => Select s_1 s_2 -> RIO<{\w1 -> true}, {\w1 v w2 -> Pure w1 w2}> s_2 @-}
 selectRight :: (Session s_2) => Select s_1 s_2 -> RIO s_2
-selectRight (Send s) = undefined 
-
--- * One-shot channels
+selectRight (Send s) = s 
 
